52eecb13c98591a8acd09deb300c928e69bdf1f6
Roman <pomanz@mail.ru>
2026-03-13 11:19:42 +0300

    new tests added

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,26 +33,26 @@ def category1() -> Category:
     return category1
 
 
-@pytest.fixture
-def category_1() -> Category:
-    """
-    Фикстура возвращает данные для проверки инициализации класса Category
-    """
-    product1 = Product("Samsung Galaxy S23 Ultra",
-
-                       "256GB, Серый цвет, 200MP камера",
-                       180000.0, 5)
-    product2 = Product("Iphone 15",
-                       "512GB, Gray space",
-                       210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11",
-
-                       "1024GB, Синий", 31000.0, 14)
-
-    return Category("Смартфоны",
-                    "Смартфоны, как средство не только коммуникации, "
-                    "но и получения дополнительных функций для удобства жизни",
-                    [product1, product2, product3])
+# @pytest.fixture
+# def category_1() -> Category:
+#     """
+#     Фикстура возвращает данные для проверки инициализации класса Category
+#     """
+#     product1 = Product("Samsung Galaxy S23 Ultra",
+#
+#                        "256GB, Серый цвет, 200MP камера",
+#                        180000.0, 5)
+#     product2 = Product("Iphone 15",
+#                        "512GB, Gray space",
+#                        210000.0, 8)
+#     product3 = Product("Xiaomi Redmi Note 11",
+#
+#                        "1024GB, Синий", 31000.0, 14)
+#
+#     return Category("Смартфоны",
+#                     "Смартфоны, как средство не только коммуникации, "
+#                     "но и получения дополнительных функций для удобства жизни",
+#                     [product1, product2, product3])
 
 
 @pytest.fixture
