01c321272a41b0edbc83a5cb2c62c5e1739a2634
Roman <pomanz@mail.ru>
2026-03-13 11:16:32 +0300

    new class added. other classes modifyed

--- src/classes.py
+++ src/classes.py
@@ -14,6 +14,16 @@ class Product:
         self.quantity = quantity
         Product.instances.append(self)
 
+    def __str__(self) -> str:
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other: 'Product') -> float:
+        """
+        :param other: Эксемпляр класса Product
+        :return: Возвращает сумму произведений цены и количества
+        """
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
     @classmethod
     def new_product(cls, product_properties: dict) -> 'Product':
         new_instance = cls(
@@ -62,12 +72,46 @@ class Category:
         Category.product_count += len(products)
         Category.category_count += 1
 
+    def __str__(self) -> str:
+        return f"{self.name}, количество продуктов: {self.product_count} шт.\n"
+
     def add_product(self, product: Product) -> None:
         self.__products.append(product)
+        Category.category_count += 1
 
     @property
     def products(self) -> str:
         result_list = ''
         for item in self.__products:
-            result_list += f"{item.name}, {item.price} руб. Остаток: {item.quantity} шт.\n"
+            result_list += f'{str(item)}\n'
         return result_list
+
+
+class ProductIterator:
+    """
+    Принимает экземпляр класса Category
+    Возвращает итератор объектов категории.
+    """
+    category: Category
+
+    def __init__(self, category: Category) -> None:
+        self.category = category
+        self.index = 0
+
+    def __iter__(self: 'ProductIterator') -> 'ProductIterator':
+        """
+        Возвращает итератор
+        """
+        return self
+
+    def __next__(self) -> str:
+        """
+        Возвращает следующее значение интератора
+        """
+        product_list = self.category.products.split('\n')
+        if self.index < int(self.category.product_count):
+            product_item = product_list[self.index]
+            self.index += 1
+            return product_item
+        else:
+            raise StopIteration
